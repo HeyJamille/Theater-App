12c8786fa5b9ef4243a18d20d38446e17a7a0ad3
Ana Angélica <anaquarele@gmail.com>
2024-07-05 11:23:38 -0300

    comingsoon componente

--- src/componentes/ComingSoon/ComingSoon.tsx
+++ src/componentes/ComingSoon/ComingSoon.tsx
@@ -1,8 +1,18 @@
 import './ComingSoon.css'
+import { filmesEmBreve } from '../../data'
+
 
 const ComingSoon = () => {
     return(
-        <h1>Coming Soon </h1>
+        <section>
+            <h1>Coming Soon</h1>
+            <div className='em-breve'>
+        {filmesEmBreve.map((filme, index)=>
+        <div key={index} className='filme'>
+            <img src={filme.image} alt={filme.title} />
+        </div> )}
+            </div>
+        </section>
     )
 }
 export default ComingSoon;
